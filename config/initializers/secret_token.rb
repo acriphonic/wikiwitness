0cbeb8d45040d798ed51cee91d18d881f933d5ba
Kendall <belleiseult@gmail.com>
2012-06-05 14:19:14 -0400

    now with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikiwitness::Application.config.secret_token = 'cd4531a4fae333c073f208fb40f42f52a3887b096408e5c3a3159eff1d0cb20221e020bec3946e4f7f89e963b90f89e139558414647e45339cab18b75d00ba04'
