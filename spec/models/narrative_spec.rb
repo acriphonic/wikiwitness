2a4331041386d0b97be244f680dcd123c0526078
Mustapha <misa@brandeis.edu>
2012-06-12 16:06:33 -0400

    narratives test

--- spec/models/narrative_spec.rb
+++ spec/models/narrative_spec.rb
@@ -1,3 +1,4 @@
+
 # == Schema Information
 #
 # Table name: narratives
@@ -13,5 +14,51 @@
 require 'spec_helper'
 
 describe Narrative do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do
+		@narrative = Narrative.new(name: "The Forgotten Samurai", location: "Karakura, Japan", 
+		content:"After a frenzied dance of crimson and steel to the wailing of fading souls 
+	  	the 9 year war was finally ended. But it was not with extraordinary strength nor with
+  		superior skill that General Yamamoto won the battle against his famed foe.....") }
+	end
+
+	subject { @narrative }
+
+	describe "field are populated" do
+		it { should respond_to(:name) }
+		it { should respond_to(:location) }
+		it { should respond_to(:content) }
+		it { should be_valid }
+	end
+
+	describe "when name is not present" do
+		before { @.name = " " }
+		it { should_not be_valid }
+    end
+
+    describe "when content is not present" do
+		before { @.content = " " }
+		it { should_not be_valid }
+    end
+
+    describe "when location is not present" do
+		before { @.location = " " }
+		it { should_not be_valid }
+    end
+
+    describe "when fields are too short" do
+		describe "with a name that is too long" do
+			before { @.name = "a" * 101 }
+			it { should_not be_valid }
+    	end
+		describe "with a location that is too long" do
+			before { @.name = "a" * 51 }
+			it { should_not be_valid }
+    	end
+    end
+
+    describe "with a content that is too short" do
+		before { @.name = "a" * 139}
+		it { should_not be_valid }
+    end
 end
+
