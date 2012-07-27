07f8668e1cae08abfdf057c13e6ad38b680a5c40
Kendall <belleiseult@gmail.com>
2012-07-27 11:17:29 -0400

    debugging

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,13 +2,7 @@ module AdminHelper
 	private
 		def correct_user
 		  @user = User.find(params[:id])
-		  if !admin_user?(@user)
-		  	puts "kitten"
-		  end
-		  if @user.account == "admin"
-		  	puts "puppy"
-		  end
-		  redirect_to(root_path) unless (current_user?(@user) || admin_user?(@user))
+		  redirect_to(root_path) unless current_user?(@user) || admin_user?(current_user)
 		end
 
 		def admin_user
